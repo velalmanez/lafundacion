e1a46b0122ce4ab581176ba2f4a0252d8dd43b8a
Ivan Velazquez <velalmanez@gmail.com>
2026-06-10 19:41:44 -0600

    update: routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,5 +7,5 @@ export const routes: Routes = [
   {path: 'about', loadComponent: () => import('./pages/about/about').then(m => m.AboutComponent)},
   {path: 'contact', loadComponent: () => import('./pages/contact/contact').then(m => m.ContactComponent)},
   {path: 'lab-cdmx', loadComponent: () => import('./pages/lab-cdmx/lab-cdmx').then(m => m.LabCdmxComponent)},
-  { path: '**', redirectTo: '' }
+  { path: '**', redirectTo: '/home' }
 ];
